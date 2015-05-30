237281b365f6e15c51082ed946d2200a60201e28
Denise <deniseschrack@gmail.com>
2015-05-29 17:28:45 -0700

    Added check out credit card validation test

--- spec/sendaletter_spec.rb
+++ spec/sendaletter_spec.rb
@@ -49,23 +49,42 @@ describe 'send a letter', type: :feature do
     expect(page.find('#letter_letter_content')).to have_content('Aaliyah, Hi. Sincerely, QA')
   end
 
-  it 'find existing inmate and pays for letter' do
-    page.fill_in 'inmate_search', with: 'Aaliyah Lawton - Delaware County PA George W. Hill Correctional Facility'
-    typeahead_select '#inmate_search', with: 'Aaliyah Lawton', index: 0
-    click_button 'Send Letter to Aaliyah'
-    page.fill_in 'letter[letter_content]', with: 'Aaliyah, Hi. Sincerely, QA'
-    click_button 'Preview and Send'
-    click_button 'Buy Now'
-    page.fill_in 'payment[first_name]', with: 'Homer'
-    page.fill_in 'payment[last_name]', with: 'Simpson'
-    page.fill_in 'payment[credit_card_number]', with: ENV['TEST_CREDIT_CARD_NUMBER']
-    select '1 - Jan', :from =>'payment[expiration_month]'
-    select '2020', :from =>'payment[expiration_year]'
-    page.fill_in 'payment[card_security_code]', with: '123'
-    page.fill_in 'payment[zip_code]', with: '99999'
-    find('.terms_and_conditions_container img').click
-    click_button 'Pay Now'
+  describe 'checking out' do
+    before :each do
+      page.fill_in 'inmate_search', with: 'Aaliyah Lawton - Delaware County PA George W. Hill Correctional Facility'
+      typeahead_select '#inmate_search', with: 'Aaliyah Lawton', index: 0
+      click_button 'Send Letter to Aaliyah'
+      page.fill_in 'letter[letter_content]', with: 'Aaliyah, Hi. Sincerely, QA'
+      click_button 'Preview and Send'
+      click_button 'Buy Now'
+    end
 
-    expect(page).to have_content('Thank you, your payment was successful and we will send your letter shortly!')
+    it 'find existing inmate and pays for letter' do
+      page.fill_in 'payment[first_name]', with: 'Homer'
+      page.fill_in 'payment[last_name]', with: 'Simpson'
+      page.fill_in 'payment[credit_card_number]', with: ENV['TEST_CREDIT_CARD_NUMBER']
+      select '1 - Jan', :from =>'payment[expiration_month]'
+      select '2020', :from =>'payment[expiration_year]'
+      page.fill_in 'payment[card_security_code]', with: '123'
+      page.fill_in 'payment[zip_code]', with: '99999'
+      find('.terms_and_conditions_container img').click
+      click_button 'Pay Now'
+
+      expect(page).to have_content('Thank you, your payment was successful and we will send your letter shortly!')
+    end
+
+    it 'find existing inmate and cannot process payment' do
+      page.fill_in 'payment[first_name]', with: 'Homer'
+      page.fill_in 'payment[last_name]', with: 'Simpson'
+      page.fill_in 'payment[credit_card_number]', with: '1111111111111'
+      select '1 - Jan', :from =>'payment[expiration_month]'
+      select '2020', :from =>'payment[expiration_year]'
+      page.fill_in 'payment[card_security_code]', with: '123'
+      page.fill_in 'payment[zip_code]', with: '99999'
+      find('.terms_and_conditions_container img').click
+      click_button 'Pay Now'
+
+      expect(page).to have_content('The credit card information you provided is not valid. Please double check the information you provided and then try again.')
+    end
   end
 end
